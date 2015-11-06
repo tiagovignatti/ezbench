0dc8a131340d10e4cb02891330ae60acf43c3beb
Martin Peres <martin.peres@linux.intel.com>
2015-11-06 12:37:38 +0200

    env_dump: dump the GL, EGL and GLX information

--- utils/env_dump/gl.c
+++ utils/env_dump/gl.c
@@ -24,14 +24,17 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#define _GNU_SOURCE
+
 #include "env_dump.h"
 
+#include <pthread.h>
+#include <EGL/egl.h>
+#include <GL/glx.h>
+#include <stdlib.h>
 #include <link.h>
 
 #if 0
-#include <GL/glx.h>
-#include <EGL/egl.h>
-
 void
 glXSwapBuffers(Display *dpy, GLXDrawable drawable)
 {
@@ -56,6 +59,122 @@ eglSwapBuffers(EGLDisplay display, EGLSurface surface)
 	return orig_eglSwapBuffers(display, surface);
 }
 #endif
+
+static pthread_mutex_t dumped_contexts_mp = PTHREAD_MUTEX_INITIALIZER;
+size_t dumped_glxcontexts_count = 0;
+GLXContext *dumped_glxcontexts;
+size_t dumped_eglcontexts_count = 0;
+EGLContext *dumped_eglcontexts;
+
+static void
+dump_gl_info()
+{
+	GLint num_extension;
+
+	/* give informations about the context */
+	glGetIntegerv(GL_NUM_EXTENSIONS, &num_extension);
+	fprintf(env_file, "GL_NEWCONTEXTUSED,%s,%s,%s,%s,%i,%s\n",
+		glGetString(GL_VENDOR), glGetString(GL_RENDERER),
+		glGetString(GL_VERSION),
+		glGetString(GL_SHADING_LANGUAGE_VERSION), num_extension,
+		glGetString(GL_EXTENSIONS));
+}
+
+Bool
+glXMakeCurrent(Display *dpy, GLXDrawable drawable, GLXContext ctx)
+{
+	static Bool (*orig_glXMakeCurrent)(Display *, GLXDrawable, GLXContext);
+	Bool ret = False;
+	int i;
+
+	pthread_mutex_lock(&dumped_contexts_mp);
+
+	if (orig_glXMakeCurrent == NULL)
+		orig_glXMakeCurrent = dlsym(RTLD_NEXT, "glXMakeCurrent");
+
+	ret = orig_glXMakeCurrent(dpy, drawable, ctx);
+	if (ret == False)
+		goto done;
+
+	/* check if the context is in the list */
+	for(i = 0; i < dumped_glxcontexts_count; i++) {
+		if (dumped_glxcontexts[i] == ctx)
+			goto done;
+	}
+
+	/* we did not find it, add it to the list before dumping all the
+	 * informations. Allocate 10 contexts at a time to avoid copying every
+	 * time.
+	 */
+	dumped_glxcontexts = realloc(dumped_glxcontexts,
+				     (((dumped_glxcontexts_count + 1) / 10) + 1) * 10);
+	dumped_glxcontexts[dumped_glxcontexts_count] = ctx;
+	dumped_glxcontexts_count++;
+
+	/* dump the egl-related informations */
+	fprintf(env_file, "GLX_NEWCONTEXTUSED,%s,%s,%s\n",
+		glXGetClientString(dpy, GLX_VENDOR),
+		glXGetClientString(dpy, GLX_VERSION),
+		glXGetClientString(dpy, GLX_EXTENSIONS));
+
+	/* dump the gl-related informations */
+	dump_gl_info();
+
+done:
+	pthread_mutex_unlock(&dumped_contexts_mp);
+	return ret;
+}
+
+EGLBoolean
+eglMakeCurrent(EGLDisplay display, EGLSurface draw, EGLSurface read,
+	       EGLContext context)
+{
+	static EGLBoolean (*orig_eglMakeCurrent)(Display *, EGLSurface,
+						 EGLSurface, EGLContext);
+	EGLBoolean ret = False;
+	EGLenum api;
+	int i;
+
+	pthread_mutex_lock(&dumped_contexts_mp);
+
+	if (orig_eglMakeCurrent == NULL)
+		orig_eglMakeCurrent = dlsym(RTLD_NEXT, "eglMakeCurrent");
+
+	ret = orig_eglMakeCurrent(display, draw, read, context);
+	if (ret == False)
+		goto done;
+
+	/* check if the context is in the list */
+	for(i = 0; i < dumped_eglcontexts_count; i++) {
+		if (dumped_eglcontexts[i] == context)
+			goto done;
+	}
+
+	/* we did not find it, add it to the list before dumping all the
+	 * informations. Allocate 10 contexts at a time to avoid copying every
+	 * time.
+	 */
+	dumped_eglcontexts = realloc(dumped_eglcontexts,
+				     (((dumped_eglcontexts_count + 1) / 10) + 1) * 10);
+	dumped_eglcontexts[dumped_eglcontexts_count] = context;
+	dumped_eglcontexts_count++;
+
+	/* dump the egl-related informations */
+	fprintf(env_file, "EGL_NEWCONTEXTUSED,%s,%s,%s,%s\n",
+		eglQueryString(display, EGL_VENDOR),
+		eglQueryString(display, EGL_VERSION),
+		eglQueryString(display, EGL_CLIENT_APIS),
+		eglQueryString(display, EGL_EXTENSIONS));
+
+	/* dump the gl-related informations */
+	api = eglQueryAPI();
+	if (api == EGL_OPENGL_API || api == EGL_OPENGL_ES_API)
+		dump_gl_info();
+
+done:
+	pthread_mutex_unlock(&dumped_contexts_mp);
+	return ret;
+}
 
 void
 _env_dump_gl_init()
