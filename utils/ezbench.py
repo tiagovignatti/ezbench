tiagovignatti/ezbench

commit 0e769d56027d5c55d894847b331947f0a345ca71

ezbench.py/git_history: parse the date along with the sha1

utils/ezbench.py
@@ -26,6 +26,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 from email.utils import parsedate_tz, mktime_tz
+from collections import namedtuple
 from datetime import datetime
 from array import array
 from enum import Enum
@@ -576,20 +577,24 @@ class SmartEzbench:
         run_info = ezbench.run_commits(["HEAD"], [], [], dry_run=True)
 
         # Get the list of commits and store their position in the list in a dict
-        git_history = subprocess.check_output(["/usr/bin/git", "log", "--format=%h"],
-                                            cwd=run_info.repo_dir).decode().split()
+        output = subprocess.check_output(["/usr/bin/git", "log", "--format=%h %ct"],
+                                          cwd=run_info.repo_dir).decode().split('\n')
+
+        GitCommit = namedtuple('GitCommit', 'sha1 timestamp')
+        for line in output:
+            fields = line.split(' ')
+            if len(fields) == 2:
+                git_history.append(GitCommit(fields[0], fields[1]))
 
         return git_history
 
-    def report(self, git_history=None, reorder_commits = True):
+    def report(self, git_history=list(), reorder_commits = True):
         if reorder_commits and git_history is None:
             git_history = self.git_history()
-        else:
-            git_history = list()
 
         # Generate the report, order commits based on the git history
         r = genPerformanceReport(self.state['log_folder'], silentMode = True,
-                                 commits_rev_order=git_history)
+                                 commits_rev_order=[c.sha1 for c in git_history])
         return r
 
     def __find_middle_commit(self, git_history, old, new, msg):
@@ -612,7 +617,7 @@ class SmartEzbench:
         if git_history is None:
             git_history = self.git_history()
         r = genPerformanceReport(self.state['log_folder'], silentMode = True,
-                                 commits_rev_order=git_history)
+                                 commits_rev_order=[c.sha1 for c in git_history])
 
         # Check all the commits
         bench_prev = dict()
@@ -865,7 +870,7 @@ def readNotes():
     except:
         return []
 
-def genPerformanceReport(log_folder, silentMode = False, commits_rev_order = dict()):
+def genPerformanceReport(log_folder, silentMode = False, commits_rev_order = list()):
     benchmarks = []
     commits = []
     labels = dict()

commit 1d61d7874828394c86de8f3a7dd078652d2a46e5

smart_ezbench: fix the auto-bisect, broken by 0e769d56
It got broken by commit:
0e769d56 "ezbench.py/git_history: parse the date along with the sha1"

utils/ezbench.py
@@ -623,8 +623,9 @@ class SmartEzbench:
         # Generate the report, order commits based on the git history
         if git_history is None:
             git_history = self.git_history()
+        commits_rev_order = [c.sha1 for c in git_history]
         r = genPerformanceReport(self.log_folder, silentMode = True,
-                                 commits_rev_order=[c.sha1 for c in git_history])
+                                 commits_rev_order=commits_rev_order)
 
         # Check all the commits
         bench_prev = dict()
@@ -641,7 +642,7 @@ class SmartEzbench:
                     msg = "The build got broken"
                 else:
                     msg = "The build got fixed"
-                middle_commit = self.__find_middle_commit(git_history,
+                middle_commit = self.__find_middle_commit(commits_rev_order,
                                                           commit_prev.sha1,
                                                           commit.sha1, msg)
                 if middle_commit is not None:
@@ -682,7 +683,7 @@ class SmartEzbench:
                         diff < (1 - perf_change_threshold)):
                         msg = "Bench '{}' went from {} to {} {}".format(bench, round(old_perf, 2),
                                                                         round(perf, 2), bench_unit)
-                        middle_commit = self.__find_middle_commit(git_history, old_commit,
+                        middle_commit = self.__find_middle_commit(commits_rev_order, old_commit,
                                                                   commit.sha1, msg)
                         if middle_commit is not None:
                             # TODO: Figure out how many runs we need based on the variance