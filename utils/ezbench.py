0b52f1d41007f92ce9f7a4628af6ccd51dd85eb9
Martin Peres <martin.peres@linux.intel.com>
2016-01-28 20:22:56 +0200

    smart_ezbench: fix the scheduling of more runs when the variance is high
    The previous code was expecting that $runs would contain the runs to
    add when it actually meant that at least that many runs should be
    made.

--- utils/ezbench.py
+++ utils/ezbench.py
@@ -719,16 +719,18 @@ class SmartEzbench:
             elif type(e) is EventInsufficientSignificance:
                 commit_sha1 = e.result.commit.sha1
                 benchmark = e.result.benchmark
-                missing_runs = max(2, e.wanted_n() - len(e.result.data)) # Schedule at least 2 more runs
-                severity = min(missing_runs / len(e.result.data), 1)
+                cur_runs = len(e.result.data)
+                wanted_runs = max(cur_runs + 2, e.wanted_n()) # schedule at least 2 more runs
+                missing_runs = wanted_runs - cur_runs
+                severity = min(missing_runs / cur_runs, 1)
                 event_prio = 1
 
                 bench_name_to_run = benchmark.full_name
-                runs = min(20, missing_runs) # cap the maximum amount of runs to play nice
+                runs = min(cur_runs + 20, wanted_runs) # cap the maximum amount of runs to play nice
 
                 # Make sure we do not schedule more than the maximum amount of run
-                if len(e.result.data) + runs > max_run_count:
-                    runs = max_run_count - len(e.result.data)
+                if runs > max_run_count:
+                    runs = max_run_count - cur_runs
                     if runs == 0:
                         continue
 
