2a2a994903ffb63751f35c5a1136f2b758dac05b
Martin Peres <martin.peres@linux.intel.com>
2016-03-16 20:09:09 +0200

    env_dump: add the exit code
    This is not perfect ... but better than nothing!

--- utils/env_dump/env_dump.sh
+++ utils/env_dump/env_dump.sh
@@ -11,4 +11,20 @@ dump_file="$1"
 shift
 
 LD_PRELOAD="$so_path" ENV_DUMP_FILE="$dump_file" "$@"
-exit $?
+exit_code=$?
+
+# Add the exit code
+if [[ "$dump_file" != "stderr" ]]
+then
+	# Do not add the EXIT_CODE line if it already exists. It means the file
+	# existed before we ran the tool. Just warn about this issue and do nothing.
+	if [ -z "$(grep EXIT_CODE "$dump_file")" ];
+	then
+		line="EXIT_CODE,$exit_code\n"
+		sed -i "s\`^-- Env dump end --\`${line}-- Env dump end --\`g" $dump_file
+	else
+		echo "ENV_DUMP: The file '$dump_file' already contains the EXIT_CODE, ignore..." >&2
+	fi
+fi
+
+exit $exit_code
